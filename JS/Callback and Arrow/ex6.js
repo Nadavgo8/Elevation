103e0d2a83b6ef675b3f6195f39dff3e2b8216d8
Nadavgo8 <19nadav99@gmail.com>
2025-07-09 14:40:50 +0300

    complete

---
+++ JS/Callback and Arrow/ex6.js
@@ -0,0 +1,13 @@
+const determineWeather = (temp) => {
+  if (temp > 25) {
+    return "hot";
+  }
+  return "cold";
+};
+
+const commentOnWeather = (temp) => "It's " + determineWeather(temp);
+
+commentOnWeather(30) //returns "It's hot"
+commentOnWeather(22) //returns "It's cold"
+console.log(commentOnWeather(30));
+console.log(commentOnWeather(22));
