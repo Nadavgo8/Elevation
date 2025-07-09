dabfffc658a728f1439a8419be58eaaf4f4e9a10
Nadavgo8 <19nadav99@gmail.com>
2025-07-09 14:32:02 +0300

    completed ex1, ex2, ex3

---
+++ JS/Callback and Arrow/ex2.js
@@ -0,0 +1,11 @@
+const returnTime = function (time) {
+  console.log("The current time is: " + time);
+};
+
+const getTime = function(callback){
+    const time = new Date();
+    callback(time);
+}
+
+getTime(returnTime);
+
---
+++ JS/Callback and Arrow/examples.js
@@ -0,0 +1,20 @@
+const timer = function () {
+  console.log(new Date());
+};
+
+// setInterval(timer, 1000);
+
+// setInterval(function () {
+//   console.log(new Date());
+// }, 1000);
+
+const square = (x) => console.log(x ** 2);
+square(4);
+const add = (x, y) => x + y;
+
+// const sum = add(1, 2)
+// console.log(sum) //prints 3
+
+// const getFormalTitle = (first, last) => {return first + " " + last;}
+// formalTitle = getFormalTitle("Madamme", "Lellouche")
+// console.log(formalTitle) //returns "Maddame Lellouche"
