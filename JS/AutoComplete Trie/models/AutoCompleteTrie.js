d469e789bfb8f0d3020367bbfb3548984a7b6d0e
Nadavgo8 <19nadav99@gmail.com>
2025-07-22 15:13:00 +0300

    created file structure

---
+++ JS/AutoComplete Trie/models/AutoCompleteTrie.js
@@ -0,0 +1,11 @@
+class AutoCompleteTrie {
+    constructor(value, endOfWord = false){
+        this.value = value;
+        this.children = {};
+        this.endOfWord = endOfWord;
+    }
+    addWord(word){
+
+    }
+
+}
