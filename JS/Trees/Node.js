d6e8674e1887af04a4a16d41b2993d114a1b45e3
Nadavgo8 <19nadav99@gmail.com>
2025-07-22 10:37:59 +0300

    added ex3, need to fix

--- JS/Trees/Node.js
+++ JS/Trees/Node.js
@@ -23,50 +23,205 @@ class Node {
     }
   }
 }
+
+
+
+
+
 class BSNode {
   constructor(value) {
     this.value = value;
-    this.leftChild;
-    this.rightChild;
+    this.leftChild = null;
+    this.rightChild = null;
   }
+
   insertNode(newVal) {
-    if (!this.value) {
-      this.value = newVal;
-    } else if (newVal > this.value && this.rightChild) {
-      this.rightChild.insertNode(newVal);
-    } else if (newVal <= this.value && this.leftChild) {
-      this.leftChild.insertNode(newVal);
-    } else if (newVal <= this.value) {
-      this.leftChild = new BSNode(newVal);
+    if (newVal <= this.value) {
+      if (this.leftChild) {
+        this.leftChild.insertNode(newVal);
+      } else {
+        this.leftChild = new BSNode(newVal);
+      }
     } else {
-      this.rightChild = new BSNode(newVal);
+      if (this.rightChild) {
+        this.rightChild.insertNode(newVal);
+      } else {
+        this.rightChild = new BSNode(newVal);
+      }
     }
   }
-  findNode(toFind) {
-    if (this == null) {
-      return false;
-    }
 
-    if (toFind === this.value) {
-      return true;
+  getMinNode() {
+    let current = this;
+    while (current.leftChild) {
+      current = current.leftChild;
     }
-
-    if (toFind > this.value) {
-      return this.rightChild ? this.rightChild.findNode(toFind) : false;
-    } else {
-      return this.leftChild ? this.leftChild.findNode(toFind) : false;
-    }
+    return current;
   }
-  findCommonParent(val1, val2) {
-    if (val1 < this.value && val2 < this.value) {
-      return this.leftChild.findCommonParent(val1, val2);
+
+  removeNode(parent, value) {
+    if (value < this.value && this.leftChild) {
+      return this.leftChild.removeNode(this, value);
+    } else if (value > this.value && this.rightChild) {
+      return this.rightChild.removeNode(this, value);
+    } else if (value !== this.value) {
+      return false; // Not found
     }
-    if (val1 > this.value && val2 > this.value) {
-      return this.rightChild.findCommonParent(val1, val2);
+
+    // Node found
+    if (!this.leftChild && !this.rightChild) {
+      if (parent) {
+        if (parent.leftChild === this) parent.leftChild = null;
+        else parent.rightChild = null;
+      }
+    } else if (this.leftChild && !this.rightChild) {
+      if (parent) {
+        if (parent.leftChild === this) parent.leftChild = this.leftChild;
+        else parent.rightChild = this.leftChild;
+      }
+    } else if (!this.leftChild && this.rightChild) {
+      if (parent) {
+        if (parent.leftChild === this) parent.leftChild = this.rightChild;
+        else parent.rightChild = this.rightChild;
+      }
+    } else {
+      // Two children: find smallest value in right subtree
+      let successor = this.rightChild;
+      let successorParent = this;
+
+      while (successor.leftChild) {
+        successorParent = successor;
+        successor = successor.leftChild;
+      }
+
+      this.value = successor.value;
+
+      if (successorParent.leftChild === successor) {
+        successorParent.leftChild = successor.rightChild;
+      } else {
+        successorParent.rightChild = successor.rightChild;
+      }
     }
-    return this.value;
+
+    return true;
+  }
+
+  findMinValue() {
+    let current = this;
+    while (current.leftChild) {
+      current = current.leftChild;
+    }
+    return current.value;
+  }
+  copyNode(node) {
+    this.value = node.value;
+    this.leftChild = node.leftChild;
+    this.rightChild = node.rightChild;
+  }
+  printInOrder() {
+    if (this.leftChild) this.leftChild.printInOrder();
+    console.log(this.value);
+    if (this.rightChild) this.rightChild.printInOrder();
   }
 }
+
+
+
+
+// class BSNode {
+//   constructor(value) {
+//     this.value = value;
+//     this.leftChild = null;
+//     this.rightChild = null;
+//   }
+//   insertNode(newVal) {
+//     if (!this.value) {
+//       this.value = newVal;
+//     } else if (newVal > this.value && this.rightChild) {
+//       this.rightChild.insertNode(newVal);
+//     } else if (newVal <= this.value && this.leftChild) {
+//       this.leftChild.insertNode(newVal);
+//     } else if (newVal <= this.value) {
+//       this.leftChild = new BSNode(newVal);
+//     } else {
+//       this.rightChild = new BSNode(newVal);
+//     }
+//   }
+//   findNode(toFind) {
+//     if (this == null) {
+//       return false;
+//     }
+
+//     if (toFind === this.value) {
+//       return true;
+//     }
+
+//     if (toFind > this.value) {
+//       return this.rightChild ? this.rightChild.findNode(toFind) : false;
+//     } else {
+//       return this.leftChild ? this.leftChild.findNode(toFind) : false;
+//     }
+//   }
+//   findCommonParent(val1, val2) {
+//     if (val1 < this.value && val2 < this.value) {
+//       return this.leftChild.findCommonParent(val1, val2);
+//     }
+//     if (val1 > this.value && val2 > this.value) {
+//       return this.rightChild.findCommonParent(val1, val2);
+//     }
+//     return this.value;
+//   }
+//   removeNode(value, parent = null) {
+//     if (value < this.value && this.leftChild) {
+//       return this.leftChild.removeNode(value, this);
+//     } else if (value > this.value && this.rightChild) {
+//       return this.rightChild.removeNode(value, this);
+//     } else if (value === this.value) {
+//       // Case 1 & 2: Node with 0 or 1 child
+//       if (!this.leftChild && !this.rightChild) {
+//         if (parent) {
+//           if (parent.leftChild === this) parent.leftChild = null;
+//           else parent.rightChild = null;
+//         }
+//       } else if (this.leftChild && !this.rightChild) {
+//         if (parent) {
+//           if (parent.leftChild === this) parent.leftChild = this.leftChild;
+//           else parent.rightChild = this.leftChild;
+//         } else {
+//           this.copyNode(this.leftChild);
+//         }
+//       } else if (!this.leftChild && this.rightChild) {
+//         if (parent) {
+//           if (parent.leftChild === this) parent.leftChild = this.rightChild;
+//           else parent.rightChild = this.rightChild;
+//         } else {
+//           this.copyNode(this.rightChild);
+//         }
+//       }
+//       // Case 3: Node with 2 children
+//       else {
+//         let minNode = this.rightChild.getMinNode();
+//         this.value = minNode.value;
+//         this.rightChild.removeNode(minNode.value, this);
+//       }
+//     }
+//     return this;
+//   }
+
+//   getMinNode() {
+//     let current = this;
+//     while (current.leftChild) {
+//       current = current.leftChild;
+//     }
+//     return current;
+//   }
+
+//   copyNode(node) {
+//     this.value = node.value;
+//     this.leftChild = node.leftChild;
+//     this.rightChild = node.rightChild;
+//   }
+// }
 
 // const letters = ["H", "E", "S", "G", "L", "Y", "I"];
 
--- JS/Trees/exercises.js
+++ JS/Trees/exercises.js
@@ -1,9 +1,9 @@
 const { Node, BSNode } = require("./Node.js");
-const letters = ["J", "H", "R", "E", "S", "P", "G", "B", "L", "Y", "I"];
+// const letters = ["J", "H", "R", "E", "S", "P", "G", "B", "L", "Y", "I"];
 
-let bSTree = new BSNode();
+// let bSTree = new BSNode();
 
-letters.forEach((l) => bSTree.insertNode(l));
+// letters.forEach((l) => bSTree.insertNode(l));
 
 // console.log(bSTree.findNode("H")); // true
 // console.log(bSTree.findNode("G")); // true
@@ -11,8 +11,47 @@ letters.forEach((l) => bSTree.insertNode(l));
 // console.log(bSTree.findNode("F")); // false
 // console.log(bSTree.findNode("y")); // false (case-sensitive)
 
-console.log(`findCommonParent("B", "I") => ${bSTree.findCommonParent("B", "I")}`); // "H"
-console.log(`findCommonParent("B", "G") => ${bSTree.findCommonParent("B", "G")}`); // "E"
-console.log(`findCommonParent("B", "L") => ${bSTree.findCommonParent("B", "L")}`); // "J"
-console.log(`findCommonParent("L", "Y") => ${bSTree.findCommonParent("L", "Y")}`); // "R"
-console.log(`findCommonParent("E", "H") => ${bSTree.findCommonParent("E", "H")}`); // "J"
+// console.log(`findCommonParent("B", "I") => ${bSTree.findCommonParent("B", "I")}`); // "H"
+// console.log(`findCommonParent("B", "G") => ${bSTree.findCommonParent("B", "G")}`); // "E"
+// console.log(`findCommonParent("B", "L") => ${bSTree.findCommonParent("B", "L")}`); // "J"
+// console.log(`findCommonParent("L", "Y") => ${bSTree.findCommonParent("L", "Y")}`); // "R"
+// console.log(`findCommonParent("E", "H") => ${bSTree.findCommonParent("E", "H")}`); // "J"
+
+
+
+
+const numbers = [8, 9, 12, 3, 5, 1, 11, 4];
+let nodeWithOneChild = new BSNode();
+numbers.forEach(n => nodeWithOneChild.insertNode(n));
+console.log(nodeWithOneChild.removeNode(nodeWithOneChild, 9)); // will return tree like the first image (the 9 will be deletied) 
+    
+let nodeWithTwoChildren = new BSNode();
+numbers.forEach(n => nodeWithTwoChildren.insertNode(n));
+console.log(nodeWithTwoChildren.removeNode(nodeWithTwoChildren, 8)); // will return tree like the second image (the root will be 5) 
+
+
+
+
+
+// const numbers = [8, 9, 12, 3, 5, 1, 11, 4];
+// let nodeWithOneChild = new BSNode();
+// numbers.forEach((n) => nodeWithOneChild.insertNode(n));
+
+// console.log("Before removing 9 (one child case):");
+// nodeWithOneChild.printInOrder();
+
+// nodeWithOneChild.removeNode(nodeWithOneChild, 9); // will return tree like the first image (the 9 will be deletied)
+
+// console.log("After removing 9:");
+// nodeWithOneChild.printInOrder();
+
+// let nodeWithTwoChildren = new BSNode();
+// numbers.forEach((n) => nodeWithTwoChildren.insertNode(n));
+
+// console.log("\nBefore removing 8 (two children case):");
+// nodeWithTwoChildren.printInOrder();
+
+// nodeWithTwoChildren.removeNode(nodeWithTwoChildren, 8); // will return tree like the second image (the root will be 5)
+
+// console.log("After removing 8:");
+// nodeWithTwoChildren.printInOrder();
