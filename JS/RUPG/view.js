9768aedec21ec4c22425ab30ac38ca447cc87eb9
Nadavgo8 <19nadav99@gmail.com>
2025-08-05 17:49:02 +0300

    add view boilerplate code

--- JS/RUPG/view.js
+++ JS/RUPG/view.js
@@ -1,1 +1,11 @@
+export default class View {
+  renderMainUser({ picture, name, location }) {}
 
+  renderQuote(quote) {}
+
+  renderPokemon({ name, image }) {}
+
+  renderAboutMe(text) {}
+
+  renderFriends(friends) {}
+}
