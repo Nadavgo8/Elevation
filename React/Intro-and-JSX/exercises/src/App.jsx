9c365ce76dfbed596b3e52589d4d7dbd3d130aeb
Nadavgo8 <19nadav99@gmail.com>
2025-08-07 14:00:08 +0300

    completed ex2

--- React/Intro-and-JSX/exercises/src/App.jsx
+++ React/Intro-and-JSX/exercises/src/App.jsx
@@ -2,28 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//ex1
+// function App() {
+//   const showCompany = (name, revenue) => { 
+//     return (
+//       <div id={name}>
+//         {name} makes {revenue} every year
+//       </div>
+//     );
+//   }
+//   const companies = [
+//     { name: "Tesla", revenue: 140 },
+//     { name: "Microsoft", revenue: 300 },
+//     { name: "Google", revenue: 600 }
+//   ];
+//   return (
+//     <div className="ex-space">
+//       <h4 className='ex-title'>Exercise 1</h4>
+//       <div className="exercise" id="ex-1">
+//         { companies.map(company => showCompany(company.name, company.revenue))}
+//       </div>
+//     </div>
+//   )
+// }
 
+
+//ex2
 function App() {
-  const showCompany = (name, revenue) => { 
-    return (
-      <div id={name}>
-        {name} makes {revenue} every year
-      </div>
-    );
-  }
-  const companies = [
-    { name: "Tesla", revenue: 140 },
-    { name: "Microsoft", revenue: 300 },
-    { name: "Google", revenue: 600 }
-  ];
+  const getClassName = (temperature) => {
+    if (temperature < 15) {
+      return "freezing";
+    }
+    if (temperature > 30) {
+      return "hell-scape";
+    }
+    return "fair";
+  };
+  const temper = 13;
   return (
     <div className="ex-space">
-      <h4 className='ex-title'>Exercise 1</h4>
-      <div className="exercise" id="ex-1">
-        { companies.map(company => showCompany(company.name, company.revenue))}
+      <h4 className="ex-title">Exercise 2</h4>
+      <div className="exercise" id="ex-2">
+        <div id="weatherBox" className={getClassName(temper)}></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
