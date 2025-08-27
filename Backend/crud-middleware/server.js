1ccefd2143cade796f07793b6c080da83da3376c
Nadavgo8 <19nadav99@gmail.com>
2025-08-27 19:42:55 +0300

    completed ex2

--- Backend/crud-middleware/server.js
+++ Backend/crud-middleware/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-
+const wordCounter = { lebron: 1 };
 
 app.get("/sanity", (req, res) => {
   res.send("Server is up and running ");
+});
+
+app.get("/:word", (req, res) => {
+  let word = req.params.word;
+  console.log(word);
+  const count = wordCounter[word] || 0;
+  res.json({ count: count });
 });
 
 
