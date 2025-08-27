77a712ffe8d16081389dd2b0c2f903e4654427d6
Nadavgo8 <19nadav99@gmail.com>
2025-08-27 19:06:11 +0300

    completed ex1

---
+++ Backend/crud-middleware/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+app.get("/sanity", (req, res) => {
+  res.send("Server is up and running ");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Running server on http://localhost:${PORT}`);
+});
