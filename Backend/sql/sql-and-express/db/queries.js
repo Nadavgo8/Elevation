05e1399a2724c19bb48ac1e3479d7589c5e0aca0
Nadavgo8 <19nadav99@gmail.com>
2025-09-04 19:06:46 +0300

    completed ex2

--- Backend/sql/sql-and-express/db/queries.js
+++ Backend/sql/sql-and-express/db/queries.js
@@ -7,16 +7,37 @@ const sequelize = new Sequelize(process.env.DB_CONNECTION, {
   logging: false,
 });
 
-async function heaviestPokemon() {
-  const [rows] = await sequelize.query(`
-    SELECT *
-    FROM pokemon
-    WHERE weight = (SELECT MAX(weight) FROM pokemon)
-  `);
-  return rows; // could be multiple if there’s a tie
+
+//ex1
+// async function heaviestPokemon() {
+//   const [rows] = await sequelize.query(`
+//     SELECT *
+//     FROM pokemon
+//     WHERE weight = (SELECT MAX(weight) FROM pokemon)
+//   `);
+//   return rows; // could be multiple if there’s a tie
+// }
+
+// (async () => {
+//   console.log(await heaviestPokemon()); // expect Snorlax in the result
+//   await sequelize.close();
+// })();
+
+//ex2
+async function findByType(typeName) {
+  const [rows] = await sequelize.query(
+    `SELECT p.name
+     FROM pokemon AS p
+     JOIN pokemon_type AS t ON t.id = p.type_id
+     WHERE t.name = ?
+     ORDER BY p.id`,
+    { replacements: [typeName] }
+  );
+  return rows.map((r) => r.name);
 }
 
+// quick test:
 (async () => {
-  console.log(await heaviestPokemon()); // expect Snorlax in the result
+  console.log(await findByType("grass")); // ["bulbasaur", "ivysaur", "venusaur", "oddish", ...]
   await sequelize.close();
 })();
